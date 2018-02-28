d884cba33737491b1b4dd9f9047cd2de03e3a4bd
Vel Georgieva <velislava.n.georgieva@gmail.com>
2018-02-28 12:03:49 +0000

    adding board and state
    using state to track turn and which square is taken up, displaying X or O on DOM

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  clicked = function (event){
-      console.log(event.target)
+  constructor(){
+    super()
+    this.state = {      
+      turn: 'X',
+      gameEnded: false,
+      board: Array(9).fill("")
+    }
   }
-
+  
+  clicked(event){
+    if(this.state.board[event.target.dataset.square] === ""){
+      this.state.board[event.target.dataset.square] = this.state.turn  
+      event.target.innerText = this.state.turn;
+      this.setState({
+        turn: this.state.turn === 'X' ? 'O' : 'X',
+        board: this.state.board
+      })
+    }    
+    console.log(this.state.board)
+  }
 
   render() {
     return (
@@ -13,16 +29,16 @@ class App extends Component {
         <div id="scoreboard">
           This is scoreboard
         </div>
-        <div id="gameboard" onClick={this.clicked}>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
+        <div id="gameboard" onClick={(e)=>this.clicked(e)}>
+          <div className="square" data-square="0"></div>
+          <div className="square" data-square="1"></div>
+          <div className="square" data-square="2"></div>
+          <div className="square" data-square="3"></div>
+          <div className="square" data-square="4"></div>
+          <div className="square" data-square="5"></div>
+          <div className="square" data-square="6"></div>
+          <div className="square" data-square="7"></div>
+          <div className="square" data-square="8"></div>
         </div>
       </div>
     );
