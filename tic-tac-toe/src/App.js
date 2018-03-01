bc9106c5c2093c3fee2ab8a0fe167a7deb8fb258
Vel Georgieva <velislava.n.georgieva@gmail.com>
2018-03-01 14:32:36 +0000

    React
    very basic AI implemented, using 'this' in every line... cannot use it when functions are declared, may assign to a const and refactor the code.

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -5,31 +5,86 @@ class App extends Component {
   constructor(){
     super()
     this.state = {      
+      winner: undefined,
+      winnerLine: ""
+    }
+    this.gameState = {
+      board: Array(9).fill(""),
       turn: 'X',
       gameEnded: false,
-      board: Array(9).fill("")
+      gameLocked: false,
+      totalMoves: 0
     }
   }
   
-  clicked(event){
-    if(this.state.board[event.target.dataset.square] === ""){
-      this.state.board[event.target.dataset.square] = this.state.turn  
-      event.target.innerText = this.state.turn;
-      this.setState({
-        turn: this.state.turn === 'X' ? 'O' : 'X',
-        board: this.state.board
-      })
+  clicked(box){
+    if(this.gameState.gameEnded || this.gameState.gameLocked) return;
+
+    if(this.gameState.board[box.dataset.square] === ""){
+      this.gameState.board[box.dataset.square] = this.gameState.turn  
+      box.innerText = this.gameState.turn;
+      this.gameState.turn = this.gameState.turn === 'X' ? 'O' : 'X'
+      
+      this.gameState.totalMoves++;
     }    
-    console.log(this.state.board)
+    const result = this.checkWinner();
+    if(result === "X"){
+      this.gameState.gameEnded = true
+      this.setState({
+        winner: "X",
+        winnerLine: "Match won by X"
+      })
+    } else if (result === "O"){
+      this.gameState.gameEnded = true
+      this.setState({
+        winner: "O",
+        winnerLine: "Match won by O!"
+      })
+    } else if (result === "draw"){
+      this.gameState.gameEnded = true
+      this.setState({
+        winner: "draw",
+        winnerLine: "Its a draw!"
+      })
+    }
+
+    if(this.gameState.turn === 'O' && !this.gameState.gameEnded) {
+      this.gameState.gameLocked = true;
+      setTimeout(()=> {
+        do {
+          var random = Math.floor(Math.random()*9);
+        } while(this.gameState.board[random] !== '');
+        this.gameState.gameLocked = false;
+        this.clicked(document.querySelectorAll('.square')[random]);
+      }, 1000);
+  
+    }
+  }  
+
+  checkWinner(){
+    const winMoves = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
+    const board = this.gameState.board
+    for(let i =0; i< winMoves.length; i++){
+      if(board[winMoves[i][0]] === board[winMoves[i][1]] &&
+      board[winMoves[i][1]] === board[winMoves[i][2]] ){
+        return board[winMoves[i][0]]
+      }
+    }
+    if(this.gameState.totalMoves === 9){
+      return "draw"
+    }
   }
 
   render() {
     return (
       <div id="game">
-        <div id="scoreboard">
-          This is scoreboard
+        <div>
+          Tic-Tac-Toe!
         </div>
-        <div id="gameboard" onClick={(e)=>this.clicked(e)}>
+        <div id="scoreboard">
+          {this.state.winnerLine}
+        </div>
+        <div id="gameboard" onClick={(e)=>this.clicked(e.target)}>
           <div className="square" data-square="0"></div>
           <div className="square" data-square="1"></div>
           <div className="square" data-square="2"></div>
